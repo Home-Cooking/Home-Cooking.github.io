a866552b94c1c4c1450571e26c49aede966e3257
Oeng Mengthong <mengthongoenggl@gmail.com>
2022-06-25 14:44:23 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1a7cd5fdf7c2838ffd67ae306c73a754",
-"index.html": "8a4998e9acf77ceaffb3bcaf3f37d527",
-"/": "8a4998e9acf77ceaffb3bcaf3f37d527",
-"main.dart.js": "e95a7bcaa32c6037d5a95886d82d88e5",
+"index.html": "3d97194abe4f6451a108b67d851bc494",
+"/": "3d97194abe4f6451a108b67d851bc494",
+"main.dart.js": "e75ef4b4ad65b6acc3e7c3435e5e1030",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
